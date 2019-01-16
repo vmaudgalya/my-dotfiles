81484aaafcb7262bbb615c86d6bb0dbf6cca8ea7
Varun Maudgalya <mvarun321@gmail.com>
2019-01-16 11:35:10 -0800

    Update custom jupyter notebook config

--- jupyter/custom/custom.js
+++ jupyter/custom/custom.js
@@ -80,3 +80,19 @@
  * @class customjs
  * @static
  */
+
+require([
+  'nbextensions/vim_binding/vim_binding',
+  'base/js/namespace',
+], function(vim_binding, ns) {
+  // Add post callback
+  vim_binding.on_ready_callbacks.push(function(){
+    var km = ns.keyboard_manager;
+    // Indicate the key combination to run the commands
+    km.edit_shortcuts.add_shortcut('ctrl-[', CodeMirror.prototype.leaveInsertMode, true);
+    km.edit_shortcuts.add_shortcut('shift-ctrl-[', CodeMirror.prototype.leaveNormalMode, true);
+
+    // Update help
+    km.edit_shortcuts.events.trigger('rebuild.QuickHelp');
+  });
+});
