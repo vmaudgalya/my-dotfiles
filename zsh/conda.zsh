9b97fd699de0d98f4f99ddf20511db0979fcd3e6
Varun Maudgalya <mvarun321@gmail.com>
2020-10-31 18:43:56 -0700

    Update conda installation

--- zsh/conda.zsh
+++ zsh/conda.zsh
@@ -1,16 +1,14 @@
-# added by Anaconda3 2019.03 installer
-# >>> conda init >>>
+# >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-# __conda_setup="$(CONDA_REPORT_ERRORS=false '/Users/vmaudgalya/anaconda3/bin/conda' shell.bash hook 2> /dev/null)"
+# __conda_setup="$('/Users/vmaudgalya/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
 # if [ $? -eq 0 ]; then
-    # \eval "$__conda_setup"
+    # eval "$__conda_setup"
 # else
-    # if [ -f "/Users/vmaudgalya/anaconda3/etc/profile.d/conda.sh" ]; then
-        # . "/Users/vmaudgalya/anaconda3/etc/profile.d/conda.sh"
-        # CONDA_CHANGEPS1=false conda activate base
-    # else
-        # \export PATH="/Users/vmaudgalya/anaconda3/bin:$PATH"
-    # fi
+if [ -f "/Users/vmaudgalya/anaconda3/etc/profile.d/conda.sh" ]; then
+    . "/Users/vmaudgalya/anaconda3/etc/profile.d/conda.sh"
+else
+    export PATH="/Users/vmaudgalya/anaconda3/bin:$PATH"
+fi
 # fi
 # unset __conda_setup
-# <<< conda init <<<
+# <<< conda initialize <<<
