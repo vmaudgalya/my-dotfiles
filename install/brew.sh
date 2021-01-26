068d13c7746ee764fd529d49e71a830e9499852e
Varun Maudgalya <mvarun321@gmail.com>
2021-01-26 11:32:01 -0800

    Use updated brew installer script

--- install/brew.sh
+++ install/brew.sh
@@ -2,7 +2,7 @@
 
 if test ! $(which brew); then
     echo "Installing homebrew"
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
 echo -e "\n\nInstalling homebrew packages..."
